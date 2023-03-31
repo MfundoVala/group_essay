1d2781a2e23391c46e2dda2ef1087394c1534318
MfundoVala <mfundosapio@gmail.com>
2023-03-31 11:04:15 +0200

    make changes

--- git_basics.py
+++ git_basics.py
@@ -1,7 +1,7 @@
 # Fill in the git commands and their descriptions in the dictionary below.
 
 git_commands = {
-    "git clone": "Makes a local copy of code in a remote repository, onto your computer.",
+    "git clone": "Makes a local copy of code in a remote repository 2 make changes, onto your computer.",
     "git add": "",
     "git commit": "",
     "git status": "",
