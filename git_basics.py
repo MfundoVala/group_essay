7827255657a80653dccb409a1251ea14a0ffec68
MfundoVala <mfundosapio@gmail.com>
2023-03-31 11:09:53 +0200

    update

--- git_basics.py
+++ git_basics.py
@@ -1,11 +1,11 @@
 # Fill in the git commands and their descriptions in the dictionary below.
 
 git_commands = {
-    "git clone": "Makes a local copy of code in a remote repository 2 make changes, onto your computer.",
-    "git add": "",
-    "git commit": "",
+    "git clone": "Makes a local copy of code in a remote repository mke changes, onto your computer.",
+    "git add": "Adds a file to the staging area.",
+    "git commit": "Saves the changes made to the fle in the staging area.",
     "git status": "",
-    "git push": "",
+    "git push": "Copies the changes made to the file in the staging area to the remote repository.",
     "git pull": "",
     "git branch": "",
     "git merge": "",
