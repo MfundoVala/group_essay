3f83edaed0a2b5d06ef028fd4107cb18cfce6362
MfundoVala <mfundosapio@gmail.com>
2023-03-31 10:46:34 +0200

    added git clone descritption

--- git_basics.py
+++ git_basics.py
@@ -1,7 +1,7 @@
 # Fill in the git commands and their descriptions in the dictionary below.
 
 git_commands = {
-    "git clone": "",
+    "git clone": "Makes a local copy of code in a remote repository, onto your computer.",
     "git add": "",
     "git commit": "",
     "git status": "",
