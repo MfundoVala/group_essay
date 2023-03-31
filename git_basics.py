df509c5f780ae98a0cdd00f7095321f8ba44d6fa
MfundoVala <mfundosapio@gmail.com>
2023-03-31 10:38:15 +0200

    first commit

---
+++ git_basics.py
@@ -0,0 +1,12 @@
+# Fill in the git commands and their descriptions in the dictionary below.
+
+git_commands = {
+    "git clone": "",
+    "git add": "",
+    "git commit": "",
+    "git status": "",
+    "git push": "",
+    "git pull": "",
+    "git branch": "",
+    "git merge": "",
+}
