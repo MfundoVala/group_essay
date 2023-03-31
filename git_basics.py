ba0ea0b847d4e8262ee9ced07d0295541e239d0a
MfundoVala <mfundosapio@gmail.com>
2023-03-31 11:10:56 +0200

    fixed errors

--- git_basics.py
+++ git_basics.py
@@ -1,7 +1,7 @@
 # Fill in the git commands and their descriptions in the dictionary below.
 
 git_commands = {
-    "git clone": "Makes a local copy of code in a remote repository mke changes, onto your computer.",
+    "git clone": "Makes a local copy of code in a remote repository make changes, onto your computer.",
     "git add": "Adds a file to the staging area.",
     "git commit": "Saves the changes made to the fle in the staging area.",
     "git status": "",
